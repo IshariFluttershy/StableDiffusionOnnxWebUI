04bebe79757465a0e69ca14d9279cc0e53ce4d43
Gally <tanguy.morvant2@gmail.com>
2022-12-21 01:56:55 +0100

    Generate an image every time the command page is visited

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,21 @@ fn index() -> &'static str {
 
 #[get("/command")]
 fn command() -> &'static str {
+  let output = Command::new("cmd")
+      .raw_arg("/C start cmd.exe /c \"cd C:\\stable_diff && .\\virtualenv\\Scripts\\activate && python txt2img_onnx.py --model .\\model\\waifu-diffusion-diffusers-onnx-v1-3 --prompt \"1girl anime smile blue_hair\" --steps 10 --scheduler eulera \"")
+      .output()
+      .expect("failed to execute process");
+
+      println!("status: {}", output.status);
+      println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+      println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
   "La commande"
-  /*if let Ok(o) = Command::new("cmd")
-    .args(["/C", "timeout 5"])
-    .output() {
-      println!("{:?}", o);
-      "Ca fait une commande !!!"
-    } else {
-      "Eh non"
-    }*/
 }
 
 #[launch]
 fn rocket() -> _ {
 
-  thread::spawn(move || {
+  /*thread::spawn(move || {
     let output = Command::new("cmd")
       .raw_arg("/C start cmd.exe /k \"cd C:\\stable_diff && .\\virtualenv\\Scripts\\activate && python onnxUI.py\"")
       .output()
@@ -35,7 +35,7 @@ fn rocket() -> _ {
       println!("status: {}", output.status);
       println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
       println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-  });
+  });*/
 
     rocket::build()
     .mount("/", routes![index])
