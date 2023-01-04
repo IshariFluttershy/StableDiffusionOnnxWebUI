f5e9ccae127f260ba6b250f8b6264995fbbc6533
Gally <tanguy.morvant2@gmail.com>
2023-01-05 00:58:11 +0100

    body variables

--- ui/src/pages/home.rs
+++ ui/src/pages/home.rs
@@ -12,6 +12,7 @@ use components::range::Range;
 
 pub enum Msg {
     OnChange(String),
+    Clicked,
 }
 
 pub struct Home {
@@ -33,10 +34,10 @@ impl Component for Home {
         Home {
             prompt: String::from("test creation home"),
             neg_prompt: String::from(""),
-            steps: 0,
-            guidance: 0.,
-            width: 0,
-            height: 0,
+            steps: 15,
+            guidance: 7.5,
+            width: 512,
+            height: 512,
         }
     }
 
@@ -47,13 +48,53 @@ impl Component for Home {
                 
         match msg {
             OnChange(input) => {
-                self.prompt = input.clone();
-                self.neg_prompt = input.clone();
+                /*self.prompt = input.clone();
+                self.neg_prompt = input.clone();*/
                 self.steps = input.parse::<u8>().unwrap();
-                self.guidance = input.parse::<f32>().unwrap();
+                /*self.guidance = input.parse::<f32>().unwrap();
                 self.width = input.parse::<u16>().unwrap();
-                self.height = input.parse::<u16>().unwrap();
+                self.height = input.parse::<u16>().unwrap();*/
 
+            }
+            Clicked => {
+                let prompt = self.prompt.clone();//String::from("test creation home");
+                let neg_prompt = self.neg_prompt.clone();
+                let steps = self.steps;
+                let guidance = self.guidance;
+                let width = self.width;
+                let height = self.height;
+
+                spawn_local(async move {
+
+                    /*let task = Task {
+                        prompt:"1girl",
+                        neg_prompt: "1boy",
+                        steps: 7,
+                        guidance: 7.5,
+                        width: 512,
+                        height: 512,
+                    };*/
+        
+                    let resp = Request::post("/command")
+                        .header("Content-Type", "application/x-www-form-urlencoded")
+                        //.body("prompt=1girl&neg_prompt=1boy&steps=7&guidance=7.5&width=512&height=512")
+                        //.body(wasm_bindgen::JsValue::from_str("prompt=bonjoure&neg_prompt=aurevoir&steps=7&guidance=7.5&width=512&height=512"))
+                        .body(wasm_bindgen::JsValue::from_str(
+                            &format!("prompt={}&neg_prompt={}&steps={}&guidance={}&width={}&height={}",
+                            prompt.clone(),
+                            neg_prompt.clone(),
+                            steps,
+                            guidance,
+                            width,
+                            height
+                            )))
+                        //.body(to_value(&task).unwrap())
+                        .send()
+                        .await
+                        .unwrap();
+                    
+                    assert_eq!(resp.status(), 200);
+                });
             }
         }
         true
@@ -62,7 +103,8 @@ impl Component for Home {
     fn view(&self, ctx: &Context<Self>) -> Html {
         
         
-        spawn_local(async {
+        let onclick = ctx.link().callback(|_| Msg::Clicked);
+        /*let onclick: Callback<Event> = Callback::from(|_| spawn_local(async {
 
             /*let task = Task {
                 prompt:"1girl",
@@ -83,8 +125,7 @@ impl Component for Home {
                 .unwrap();
             
             assert_eq!(resp.status(), 200);
-        });
-
+        }));*/
 
         
 
@@ -106,18 +147,21 @@ impl Component for Home {
                         <span class="input-group-text">{"prompt"}</span>
                         <textarea class="form-control" aria-label="prompt" name="prompt"></textarea>
                     </div>
+                    <br/>
                     <div class="input-group">
                         <span class="input-group-text">{"negative prompt"}</span>
                         <textarea class="form-control" aria-label="negative prompt" name="neg_prompt" ></textarea>
                     </div>
+                    <br/>
                     <div>
-                        <Range value=15. text={"steps"} name={"steps"} min=1. max=150. step=1. on_change={on_cautious_change.clone()}></Range>
-                        <Range value=7.5 text={"guidance"} name={"guidance"} min=1. max=25. step=0.1 on_change={on_cautious_change.clone()}></Range>
-                        <Range value=512. text={"width"} name={"width"} min=256. max=1024. step=64. on_change={on_cautious_change.clone()}></Range>
-                        <Range value=512. text={"height"} name={"height"} min=256. max=1024. step=64. on_change={on_cautious_change}></Range>
+                        <Range value=15. text={"steps"} name={"steps"} min=1. max=150. step=1. on_change={on_cautious_change.clone()}></Range> <br/>
+                        <Range value=7.5 text={"guidance"} name={"guidance"} min=1. max=25. step=0.1 on_change={on_cautious_change.clone()}></Range> <br/>
+                        <Range value=512. text={"width"} name={"width"} min=256. max=1024. step=64. on_change={on_cautious_change.clone()}></Range> <br/>
+                        <Range value=512. text={"height"} name={"height"} min=256. max=1024. step=64. on_change={on_cautious_change}></Range> <br/>
                     </div>
+                    <br/>
                     <div class="button">
-                        <button >{"Envoyer le message"}</button>
+                        <button {onclick}>{"Envoyer le message"}</button>
                     </div>
                 //</form>
             </div>
